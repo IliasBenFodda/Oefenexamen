352fde0972dab7e282771f9dadb7093b5f7a0e90
Ben-Fodda <ilias.benfodda@student.odisee.be>
2025-12-19 14:07:19 +0100

    Javadoc voor bezoeker en leeftijdscategorie

--- src/Bezoeker.java
+++ src/Bezoeker.java
@@ -1,10 +1,18 @@
 import java.time.LocalDate;
 
+/**
+ * Bezoeker is een class die gebruikt wordt om bezoekers in het dierenpark voor te stellen
+ * @author Ilias Ben-Fodda
+ *
+ */
 public class Bezoeker extends Persoon{
 
     public Bezoeker(String naam, LocalDate geboorteDatum, String adres) {
         super(naam, geboorteDatum, adres);
     }
+    /**
+     * Een methode om de leeftijdscategorie van een bezoeker op te vragen
+     */
     public Leeftijdscategorie getLeeftijdscategorie() {
         return Leeftijdscategorie.berekenLeeftijdscategorie(this.getGeboorteDatum());
     }
--- src/Leeftijdscategorie.java
+++ src/Leeftijdscategorie.java
@@ -1,12 +1,20 @@
 import java.time.LocalDate;
 import java.time.Period;
 
+/**
+ * Leeftijdscategorie is een class die gebruikt wordt om de leeftijdscategorie voor te stellen
+ * @author Ilias Ben-Fodda
+ *
+ */
 public enum Leeftijdscategorie {
     ONDER_TWAALF,
     KINDEREN,
     VOLWASENEN,
     BOVEN_65;
-
+    /**
+     * Een methode om een leeftijdscategorie van de bezoeker te berekenen
+     * @param geboorteDatum De geboortedatum van de bezoeker
+     */
     public static Leeftijdscategorie berekenLeeftijdscategorie(LocalDate geboorteDatum) {
         int leeftijd = Period.between(geboorteDatum, LocalDate.now()).getYears();
         if (leeftijd < 12) {
