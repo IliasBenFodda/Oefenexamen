f60ef3a18db3136d4ffada7a4fc30bd4fee2caa6
Ben-Fodda <ilias.benfodda@student.odisee.be>
2025-12-19 13:52:22 +0100

    Implementatie Activiteit

--- src/Activiteit.java
+++ src/Activiteit.java
@@ -1,3 +1,45 @@
-public class Activiteit {
+import java.util.Set;
 
+public class Activiteit {
+    private String naam;
+    private Set<Persooneelslid> personeelsleden;
+    private Set<Bezoeker> bezoekers;
+    private String categorie;
+    private String omschrijving;
+
+    public Activiteit(String naam, Set<Persooneelslid> personeelsleden, Set<Bezoeker> bezoekers, String categorie, String omschrijving) {
+        this.naam = naam;
+        this.personeelsleden = personeelsleden;
+        this.bezoekers = bezoekers;
+        this.categorie = categorie;
+        this.omschrijving = omschrijving;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public Set<Persooneelslid> getPersoneelsleden() {
+        return personeelsleden;
+    }
+
+    public Set<Bezoeker> getBezoekers() {
+        return bezoekers;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public String getOmschrijving() {
+        return omschrijving;
+    }
+
+    public void schrijfBezoekerIn(Bezoeker bezoeker) {
+        this.bezoekers.add(bezoeker);
+    }
+
+    public void voegPersoneelToe(Persooneelslid persooneelslid) {
+        this.personeelsleden.add(persooneelslid);
+    }
 }
