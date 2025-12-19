6d2fb47da8b07b9ec2091256bccd1e1f4a14c4ee
Ben-Fodda <ilias.benfodda@student.odisee.be>
2025-12-19 13:42:27 +0100

    Aanmaken zone + basic activiteit

---
+++ src/Zone.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.Set;
+
+public class Zone {
+    private int maximumCapaciteit;
+    private Set<Activiteit> activiteiten;
+    private List<Leeftijdscategorie> leeftijdscategorien;
+    private String naam;
+
+    public Zone(int maximumCapaciteit, Set<Activiteit> activiteiten, List<Leeftijdscategorie> leeftijdscategorien, String naam) {
+        this.maximumCapaciteit = maximumCapaciteit;
+        this.activiteiten = activiteiten;
+        this.leeftijdscategorien = leeftijdscategorien;
+        this.naam = "";
+    }
+
+    public int getMaximumCapaciteit() {
+        return maximumCapaciteit;
+    }
+
+    public Set<Activiteit> getActiviteiten() {
+        return activiteiten;
+    }
+
+    public List<Leeftijdscategorie> getLeeftijdscategorien() {
+        return leeftijdscategorien;
+    }
+
+    public void voegActiviteitToe(Activiteit activiteit) {
+        activiteiten.add(activiteit);
+    }
+
+
+}
